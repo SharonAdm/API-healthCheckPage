5975182ec837c867e9f7a14fe41d0b9e709096d1
Sharon <sharon.adami14@gmail.com>
2019-11-10 11:18:16 +0330

    "Modified App.js according to new components."

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React,{ Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/header';
-import ApiTable from './components/apiTable';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/header";
+import Table from "./components/test2";
 
-class App extends Component{
-  render(){
-    return(
+class App extends Component {
+  render() {
+    return (
       <main className="container">
         <Header />
-        <ApiTable />
+        <Table />
       </main>
-    )
+    );
   }
 }
 export default App;
