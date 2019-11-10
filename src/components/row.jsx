47b11c3f590d2945babb3435e3bfd81d27dab032
Sharon <sharon.adami14@gmail.com>
2019-11-10 11:35:01 +0330

    Handeled some minor warnings.

--- src/components/row.jsx
+++ src/components/row.jsx
@@ -2,27 +2,30 @@ import React, { Component } from "react";
 
 class Row extends Component {
   state = {
-      status: undefined
+    status: undefined
+  };
+
+  componentDidMount() {
+    fetch(this.props.row.url)
+      .then(res => res.json())
+      .then(result => {
+        if (result.status === "UP") {
+          this.setState({ status: true });
+        }
+      })
+      .catch(() => {
+        this.setState({ status: false });
+      });
   }
 
-  componentDidMount(){
-    fetch(this.props.row.url)
-    .then(res => res.json())
-    .then(result => {
-      if (result.status === "UP") {
-          this.setState({status:true});
-      }
-    }).catch(()=>{
-        this.setState({status:false});
-    });
-  }
-  
   render() {
     return (
       <tr>
         <td>{this.props.row.name}</td>
-        {this.state.status != undefined && <td>{this.state.status ? "true" : "false"}</td>}
-        {this.state.status==undefined && <td>{"-"}</td>}
+        {this.state.status !== undefined && (
+          <td>{this.state.status ? "true" : "false"}</td>
+        )}
+        {this.state.status === undefined && <td>{"-"}</td>}
       </tr>
     );
   }
