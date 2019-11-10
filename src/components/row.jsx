ba9f28ed894f5575f79d3456655cb7cc45e8800b
Sharon <sharon.adami14@gmail.com>
2019-11-10 14:50:58 +0330

    Adding icon.

--- src/components/row.jsx
+++ src/components/row.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
 
 class Row extends Component {
   state = {
@@ -23,7 +25,7 @@ class Row extends Component {
       <tr>
         <td>{this.props.row.name}</td>
         {this.state.status !== undefined && (
-          <td>{this.state.status ? "true" : "false"}</td>
+          <td>{this.state.status ? (<FontAwesomeIcon icon={faCheck}></FontAwesomeIcon>) : "false"}</td>
         )}
         {this.state.status === undefined && <td>{"-"}</td>}
       </tr>
