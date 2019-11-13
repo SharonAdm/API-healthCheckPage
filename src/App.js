aa092edd99ebd56a9704dbb9fc26337c00b23448
Sharon <sharon.adami14@gmail.com>
2019-11-13 11:37:03 +0330

    Cleaned up some extra lines of code.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 import Header from "./components/header";
 import Table from "./components/table";
 
-library.add(fab, faCoffee);
+library.add(fab);
 
 class App extends Component {
   render() {
